f5b194d2607038cc81f6ec76db68f9247f58c45a
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2025-02-02 10:06:35 +0200

    4 done entity ContractItem from AbsItem

--- sellbuy_weapons/contracts/MarketWeapons.sol
+++ sellbuy_weapons/contracts/MarketWeapons.sol
@@ -2,8 +2,13 @@
 pragma solidity >=0.4.25 <0.9.0;
 pragma experimental ABIEncoderV2;
 
+import "./abstracts/AbsItem.sol";
+import "./abstracts/ContractItem.sol";
+import "./abstracts/OwnerItem.sol";
+
 contract MarketWeapons {
     address payable private owner;
+    ContractItem private contractItem;
 
     constructor() payable {
         owner = payable(msg.sender);
@@ -12,6 +17,14 @@ contract MarketWeapons {
     modifier onlyOwner() {
         require(owner == msg.sender, "Error! Only owner can call.");
         _;
+    }
+
+    function initContractData() public onlyOwner {
+        contractItem = new ContractItem(owner, address(this), block.timestamp);
+    }
+
+    function getContractItem() external view returns(AbsItem item){
+        return contractItem;
     }
 
     function getOwnerBalance() external view onlyOwner returns (uint) {

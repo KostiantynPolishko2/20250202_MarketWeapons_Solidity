6bd6e1335977943d4db11af2aa8976fd18419ed7
KostiantynPolishko2 <polxs_wp31@student.itstep.org>
2025-02-03 19:01:32 +0200

    2 change logic to create txID

--- demo_timelock/contracts/Lock.sol
+++ demo_timelock/contracts/Lock.sol
@@ -2,11 +2,13 @@
 pragma solidity >=0.4.25 <0.9.0;
 pragma experimental ABIEncoderV2;
 
+
 contract Timelock {
-    uint constant MINIMUM_DELAY = 10;
-    uint constant MAXIMUM_DELAY = 1 days;
-    uint constant GRACE_PERIOD = 1 days;
+    // uint constant MINIMUM_DELAY = 10;
+    // uint constant MAXIMUM_DELAY = 1 days;
+    // uint constant GRACE_PERIOD = 1 days;
     address public owner;
+    address public _toContract;
     string public message;
     uint public amount;
 
@@ -17,12 +19,13 @@ contract Timelock {
         _;
     }
 
-    event Queued(bytes32 txId);
-    event Discarded(bytes32 txId);
-    event Executed(bytes32 txId);
+    event Queued(bytes32 txId, uint indexed _timestamp, string _func);
+    event Discarded(bytes32 txId, uint indexed _timestamp, string _func);
+    event Executed(bytes32 txId, uint indexed _timestamp, string _func);
 
     constructor() {
         owner = msg.sender;
+        _toContract = address(this);
     }
 
     function demo(string calldata _msg) external payable {
@@ -30,32 +33,29 @@ contract Timelock {
         amount = msg.value;
     }
 
-    function getNextTimestamp() external view returns(uint) {
-        return block.timestamp + 60;
+    function getNextTimestamp() private view returns(uint) {
+        return block.timestamp + 0;
     }
 
-    function prepareData(string calldata _msg) external pure returns(bytes memory) {
+    function prepareData(string calldata _msg) private pure returns(bytes memory) {
         return abi.encode(_msg);
     }
 
-    function addToQueue(address _to, string calldata _func, bytes calldata _data, uint _value, uint _timestamp) external onlyOwner returns(bytes32) {
-        require(_timestamp > block.timestamp + MINIMUM_DELAY &&_timestamp < block.timestamp + MAXIMUM_DELAY, "invalid timestamp");
-        bytes32 txId = keccak256(abi.encode(_to, _func, _data, _value, _timestamp));
+    function addToQueue(string calldata _func) external payable onlyOwner returns(bytes32) {
+
+        uint _timestamp = getNextTimestamp();
+        bytes32 txId = keccak256(abi.encode(_toContract, _func, _timestamp));
 
         require(!queue[txId], "already queued");
 
         queue[txId] = true;
 
-        emit Queued(txId);
+        emit Queued(txId, _timestamp, _func);
 
         return txId;
     }
 
-    function execute(address _to, string calldata _func, bytes calldata _data, uint _value, uint _timestamp) external payable onlyOwner returns(bytes memory) {
-        require(block.timestamp > _timestamp, "too early");
-        require(_timestamp + GRACE_PERIOD > block.timestamp,"tx expired");
-
-        bytes32 txId = keccak256(abi.encode(_to, _func, _data, _value, _timestamp));
+    function execute(bytes32 txId, string calldata _func, bytes calldata _data, uint _value) external payable onlyOwner returns(bytes memory) {
 
         require(queue[txId], "not queued!");
 
@@ -68,10 +68,10 @@ contract Timelock {
             data = _data;
         }
 
-        (bool success, bytes memory resp) = _to.call{value: _value}(data);
+        (bool success, bytes memory resp) = _toContract.call{value: _value}(data);
         require(success);
 
-        emit Executed(txId);
+        emit Executed(txId, block.timestamp, _func);
         return resp;
     }
 
@@ -80,6 +80,6 @@ contract Timelock {
 
         delete queue[_txId];
 
-        emit Discarded(_txId);
+        emit Discarded(_txId, block.timestamp, "demo(string)");
     }
 }
